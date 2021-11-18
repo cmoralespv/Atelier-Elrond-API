b97490b728d57b9f32bcb39eee4435c192b69543
Carlos Morales <cmorpv@gmail.com>
2021-11-17 20:04:11 -0700

    updated schema file

--- schema.sql
+++ schema.sql
@@ -24,12 +24,20 @@ CREATE TABLE characteristics (
 
 CREATE TABLE characteristic_reviews (
   id  serial  PRIMARY KEY,
-  characteristic_id  serial  REFERENCES characteristics (id)  NOT NULL,
-  review_id  serial REFERENCES reviews (id) NOT NULL,
+  characteristic_id  serial  REFERENCES characteristics (id)  ON DELETE CASCADE,
+  review_id  serial REFERENCES reviews (id)  ON DELETE CASCADE,
   value  smallint
+)
+
+
+CREATE TABLE reviews_photos.csv (
+  id serial PRIMARY KEY,
+  review_id  serial  REFERENCES reviews (id)  ON DELETE CASCADE,
+  url  varchar(300)  NOT NULL
 )
 
 
 CREATE UNIQUE INDEX reviews_pkey ON public.reviews USING btree (id)
 CREATE UNIQUE INDEX characteristics_pkey ON public.characteristics USING btree (id)
 CREATE UNIQUE INDEX characteristic_reviews_pkey ON public.characteristic_reviews USING btree (id)
+CREATE UNIQUE INDEX reviews_photos_pkey ON public.reviews_photos USING btree (id)

27e9fcbe813cace2b6f4094194c0533767ff10fe
Carlos Morales <cmorpv@gmail.com>
2021-11-20 21:03:30 -0700

    fixed eslinter messages

--- src/api/routes/get.js
+++ src/api/routes/get.js
@@ -56,18 +56,3 @@ router.get('/reviews/meta', (req, res) => {
 module.exports = router;
 
 // Relevance will be determined by a combination of both the date that the review was submitted as well as ‘helpfulness’ feedback received.  This combination should weigh the two characteristics such that recent reviews appear near the top, but do not outweigh reviews that have been found helpful.  Similarly, reviews that have been helpful should appear near the top, but should yield to more recent reviews if they are older.
-
-
-
-// sql
-// .query(`SELECT id, rating, recommend
-//   FROM reviews
-//   WHERE product_id = ${product_id}
-//   ORDER BY id ASC
-//   LIMIT 1`)
-// .then(data => {
-//   res.send(data);
-//   console.log(res.statusCode);
-// })
-// .catch(e => console.error(e.stack));
-// });
